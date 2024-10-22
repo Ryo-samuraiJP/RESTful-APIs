d181a30665922356ddbc2c191cbb5b3cee8cfab4
Rio <onlytru6340@gmail.com>
2024-10-21 23:53:36 -0700

    Create PUT endpoint

--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -21,3 +21,12 @@ export const getContacts = async (req, res) => {
     res.send(err);
   }
 };
+
+export const getContactWithID = async (req, res) => {
+  try {
+    const contacts = await Contact.findById(req.params.contactId);
+    res.json(contacts);
+  } catch (err) {
+    res.send(err);
+  }
+};
