e7a96a7caee2baaa6990c63a7ab5baf160114478
Rio <onlytru6340@gmail.com>
2024-10-22 23:40:42 -0700

    Refactor static file serving

--- index.js
+++ index.js
@@ -14,6 +14,9 @@ mongoose.connect("mongodb://localhost/CRMdb");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// servering static files
+app.use(express.static("public"));
+
 routes(app);
 
 app.get("/", (req, res) => {
